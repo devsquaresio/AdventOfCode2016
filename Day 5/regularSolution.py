93f6b07f6ce610e518fa2e76efe1655721211fb8
DevSquares <111458490+devsquaresio@users.noreply.github.com>
2023-07-02 18:05:08 -0400

    Create regularSolution.py

---
+++ Day 5/regularSolution.py
@@ -0,0 +1,20 @@
+import hashlib, string
+
+passcode, counter, txt = '', 0, 'wtnhxymk'
+passcode2, passcode2real = ['_' for i in range(0, 8)], ''
+
+while (len(passcode) < 8) or ('_' in passcode2):
+    testval = hashlib.md5(txt.encode(encoding='utf-8') + str(counter).encode(encoding='utf-8'))
+    if testval.hexdigest().startswith('00000'):
+        if len(passcode) < 8:
+            passcode += (testval.hexdigest()[5])
+        if testval.hexdigest()[5] in string.digits and int(testval.hexdigest()[5]) <= 7 and passcode2[int(testval.hexdigest()[5])] == '_':
+            passcode2[int(testval.hexdigest()[5])] = testval.hexdigest()[6]      
+    counter += 1
+
+for i in passcode2:
+    passcode2real += str(i)
+
+print("The first part of the solution to the problem is {}".format(passcode))
+print("The second part of the solution to the problem is {}".format(passcode2real))
+
